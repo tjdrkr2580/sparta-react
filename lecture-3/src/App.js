06080dbf3e2f15c2096f5e6674b87b09b744409c
tjdrkr2580 <tjdrkr2580@naver.com>
2023-02-16 19:30:21 +0900

    ADD | 5. axios get

--- lecture-3/src/App.js
+++ lecture-3/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [state, setState] = useState(null);
+  const fetchTodo = async () => {
+    const { data } = await axios.get("http://localhost:4000/todos");
+    setState(data);
+  };
+  useEffect(() => {
+    fetchTodo();
+  }, []);
+  console.log(state);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state &&
+        state.map((element) => <div key={element.id}>{element.title}</div>)}
     </div>
   );
 }
